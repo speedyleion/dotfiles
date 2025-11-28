d8c75ba0b74b1be6e23abb1a7ef0dd3d687f6b1a
Nick <speedyleion@users.noreply.github.com>
2025-11-28 12:24:39 -0800

    derp, helper method rename
    - Add `y` to yank in copy mode for tmux
    - Add `fd` ignore
    - Search for hidden in vim FZF command

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -1,7 +1,7 @@
 if vim.fn.executable('fdfind') == 1 then
-    vim.env.FZF_DEFAULT_COMMAND='fdfind --type f'
+    vim.env.FZF_DEFAULT_COMMAND='fdfind -H --type f'
 elseif vim.fn.executable('fd') == 1 then
-    vim.env.FZF_DEFAULT_COMMAND='fd --type f'
+    vim.env.FZF_DEFAULT_COMMAND='fd -H --type f'
 end
 
 return {
